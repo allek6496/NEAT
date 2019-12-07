93f852a9dae7a7311d892f675dded71bc7a49646
allek6496 <allek6496@wrdsb.ca>
2019-12-07 14:42:18 -0500

    Worked on mutation and added fitness calculation

--- NEAT.py
+++ NEAT.py
@@ -1,16 +1,21 @@
 from individual import individual
 
-# The difference between two individuals
-c1, c2, c3 = 0, 0, 0
+#how simmilar individuals within a species are
+dt = 3
+
+#constants used in the difference function
+c1, c2, c3 = 1, 1, 0.4
+
+#the difference between two individuals
 def delta(indi1, indi2):
     E, D, WSum, WNum = 0, 0, 0, 0
     N = max([20, len(indi1), len(indi2)])
     N = 1 if N == 20 else N
     indi2Genes = [gene2[4] for gene2 in indi2.genome]
     for gene in indi1.genome:
-        if gene[4] > indi2.genome[-1][4]: # if this innovation number is greater than the greatest of the other's innovation numbers, then it's excess
+        if gene[4] > indi2.genome[-1][4]: #if this innovation number is greater than the greatest of the other's innovation numbers, then it's excess
             E += 1
-        elif gene[4] not in indi2Genes: # if it's not bigger but not in there either, it's disjoint
+        elif gene[4] not in indi2Genes: #if it's not bigger but not in there either, it's disjoint
             D += 1
         else:
             WNum += 1
@@ -23,18 +28,21 @@ def game(indi1, indi2):
     snow1, snow2 = 1, 1
     ducks1, ducks2 = 5, 5
     lives1, lives2 = 3, 3
-    history = [[-1, -1]]*30
+    history = []
+    turns = 0
 
-    for i in range(30):
+    while turns < 30 and f == [0, 0]:
+        turns += 1
+
         history1, history2 = [], []
-        for j in range(0, 10):
+        for j in range(0, 9):
             try:
-                history1 += [history[i-j-1][0]]
+                history1 += [history[-(j+1)][0]]
             except IndexError:
                 history1 += [-1]
             
             try:
-                history2 += [history[i-j-1][1]]
+                history2 += [history[-(j+1)][1]]
             except IndexError:
                 history2 += [-1]
 
@@ -59,58 +67,52 @@ def game(indi1, indi2):
             if snow1 > 0:
                 snow1 -= 1
             else:
-                ch1 = -1
-                f[0] -= 5
+                f[0] = -5
         elif ch1 == 1:
             if ducks1 > 0:
                 ducks1 -= 1
             else:
-                ch1 = -1
-                f[0] -= 5
+                f[0] = -5
         elif ch1 == 2:
             if snow1 < 10:
                 snow1 += 1
             else:
-                ch1 = -1
-                f[0] -= 5
+                f[0] = -5
 
         if ch2 == 0:
             if snow2 > 0:
                 snow2 -= 1
             else:
-                ch2 = -1
-                f[1] -= 5
+                f[1] = -5
         elif ch2 == 1:
             if ducks2 > 0:
-                ducks2 -= 1
+                ducks2 = -1
             else:
-                ch2 = -1
-                f[1] -= 5
+                f[1] = -5
         elif ch2 == 2:
             if snow2 < 10:
                 snow2 += 1
             else:
-                ch2 = -1
-                f[1] -= 5
+                f[1] = -5
         
-        if ch1 == 0 and (ch2 in [-1, 2]):
+        if ch1 == 0 and ch2 == 2:
             lives2 -= 1
-        elif ch2 == 0 and (ch1 in [-1, 2]):
+        elif ch2 == 0 and ch1 == 2:
             lives1 -= 1
 
-        if lives1 == 0:
-            f[0] += 10
-            f[1] -= 10
-            return f
-        elif lives2 == 0:
-            f[1] += 10
-            f[0] -= 10
-            return f
+        if f == [0, 0]:
+            if lives1 == 0:
+                f[0] = -2
+                f[1] = 1
+                return f
+            elif lives2 == 0:
+                f[1] = 1
+                f[0] = -2
+                return f
 
-    f[0], f[1] = f[0]-5, f[1]-5
     return f
 
-pop = [individual() for i in range(150)] # creates a bunch of blank individuals
+pop = [individual() for i in range(150)] #creates a bunch of blank individuals
 fitnesses = [0]*150
 
 global innoNum
@@ -119,9 +121,37 @@ for indi in pop:
     for i in range(10):
         innoNum = indi.mutateStructure(150, innoNum)
 
-for indi1 in pop:
-    for indi2 in pop:
-        # play a game 20 times and update that individual's fitness
-        # i'd imagine it's fine to just add or subtract to the one number based on how well it did
-        pass
+#store each of the different species
+specs = []
 
+for i in range(len(pop)):
+    for spec in specs:
+        if delta(pop[i], pop[spec[0]]) < dt:
+            spec.append(i)
+            break
+    else:
+        specs.append([i])
+
+#runs each individual against another individual and updates their fitness
+for i1 in range(len(pop)):
+    for i2 in range(i1+1, len(pop)):
+        #there is no need to play any two bots more than once because the same thing will happen each time (no randomness)
+
+        results = game(pop[i1], pop[i2])
+        fitnesses[i1] += results[0]
+        fitnesses[i2] += results[1]
+        
+#adjusts the fitnesses for the species size
+for i in range(len(fitnesses)):
+    #search for the species this individual is in
+    s = 0
+    for spec in specs:
+        try:
+            s = spec.index(i)
+            break
+        except IndexError:
+            continue
+    
+    fitnesses[i] = fitnesses[i]/len(specs[s])
+
+#TODO: add crossover and future population creation
--- individual.py
+++ individual.py
@@ -1,5 +1,5 @@
 from math import e
-from random import uniform
+from random import uniform, choice
 import numpy as np
 
 '''
@@ -31,12 +31,12 @@ Outputs:
 '''
 class individual:
 	def __init__(self, genome=None):
-		self.maxMutation = 0.1 #Max amount a weight can be changed by
-		self.largePopulation = 15 #What constitutes a large species
-		self.maxRandom = 3 #How random the totally random mutation is
+		self.maxMutation = 0.1 #max amount a weight can be changed by
+		self.largePopulation = 15 #what constitutes a large species
+		self.maxRandom = 3 #how random the totally random weight is
 
 		self.genome = []
-		self.nodes = ["i"]*17 + ["o"]*3 #The list of nodes and their types
+		self.nodes = ["i"]*17 + ["o"]*3 #the list of nodes and their types
 
 		for gene in genome:
 			self.genome += gene
@@ -56,12 +56,12 @@ class individual:
 
 		return decisions
 
-	#Activation function, maps from -inf -> +inf to 0 -> 1
+	#activation function, maps from -inf -> +inf to 0 -> 1
 	@staticmethod
 	def activate(sum):
 		return 1/(1+e**(-4.9*sum))
 
-	#Recursive function that finds the value of a node
+	#recursive function that finds the value of a node
 	def getNode(self, node, inputs):
 		sum = 0
 		for gene in self.genome:
@@ -73,25 +73,64 @@ class individual:
 		return self.activate(sum)
 
 	def mutateWeights(self):
-		for gene in self.genome:
-			if uniform(0, 1) < 0.8:
+		#80% chance to be changed
+		if uniform(0, 1) < 0.8:
+
+			#go through each gene
+			for gene in self.genome:
+
+				#90% to change by a small amount
 				if uniform(0, 1) < 0.9:
 					gene[2] += uniform (-1, 1)*self.maxMutation
+				#10% to be totally different
 				else:
 					gene[2] = uniform(-1, 1)*self.maxRandom
 			
 	def mutateStructure(self, popSize, previousNum):
-		if uniform(0, 1) < 0.03:
+		if uniform(0, 1) < 0.03 and len(self.genome) > 0:
 			self.nodes += ['h']
-			self.genome += []
-			pass
+
+			#pick a random enabled gene to split into two
+			gene = choice([g for g in self.genome if g[3] == 1])
+			
+			#disable this gene
+			gene[3] = 0 
+
+			#make a new gene going to the node we just added with weight 1
+			self.genome.append([gene[0], len(self.nodes)-1, 1, 1, previousNum + 1])
+
+			#make a gene coming from the new node with the old weight
+			self.genome.append([len(self.nodes)-1, gene[1], gene[2], 1, previousNum + 2])
+			
+			#two numbers were used so increment by two
+			return previousNum + 2
+			
 		elif popSize >= self.largePopulation and uniform(0, 1) < 0.3:
-			#add link with random weight
-			pass
+			self.genome.append([])
+			
+			#pick a random non-output node to come from
+			self.genome[-1].append(choice([e.index for e in self.nodes if e != 'o']))
+
+			#pick a random non-input node to go to
+			self.genome[-1].append(choice([e.index for e in self.nodes if e != 'i']))
+
+			#add a random weight to this node
+			self.genome[-1].append(uniform(-1, 1)*self.maxRandom)
+
+			#set it to be enabled
+			self.genome[-1].append(1)
+
+			#give it the new innovation number
+			self.genome[-1].append(previousNum + 1)
+			return previousNum + 1
 		elif uniform(0, 1) < 0.05:
-			#add link with random weight
-			pass
+			#same code as in the previous part
+			self.genome.append([])
+			self.genome[-1].append(choice([e.index for e in self.nodes if e != 'o']))
+			self.genome[-1].append(choice([e.index for e in self.nodes if e != 'i']))
+			self.genome[-1].append(uniform(-1, 1)*self.maxRandom)
+			self.genome[-1].append(1)
+			self.genome[-1].append(previousNum + 1)
+			return previousNum + 1
 		else:
 			return previousNum
-		
-		return previousNum + 1
